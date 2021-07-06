210532635f9a1724b9e3beecf328e99924603d23
run-after <run.after@protonmail.com>
2021-07-06 14:27:25 -0500

    Add postController

---
+++ controllers/postController.js
@@ -0,0 +1,31 @@
+const Post = require('../models/Post');
+
+//GET /posts
+module.exports.post_list = (req, res, next) => {
+
+};
+
+//POST /posts
+module.exports.create_post = (req, res, next) => {
+
+};
+
+//GET /posts/:postID
+module.exports.post_detail = (req, res, next) => {
+
+};
+
+//PUT /posts/:postID
+module.exports.edit_post = (req, res, next) => {
+
+};
+
+//DELETE /posts/:postID
+module.exports.delete_post = (req, res, next) => {
+
+};
+
+//PUT /posts/:postID/like
+module.exports.like_post = (req, res, next) => {
+
+};
