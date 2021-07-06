e2a56f556faf31dddc3182a1f3066800ad1563e3
run-after <run.after@protonmail.com>
2021-07-06 14:27:50 -0500

    Add comment controller

---
+++ controllers/commentController.js
@@ -0,0 +1,31 @@
+const Comment = require('../models/Comment');
+
+//GET /posts/:postID/comments
+module.exports.comment_list = (req, res, next) => {
+
+};
+
+//POST /posts/:postID/comments
+module.exports.create_comment = (req, res, next) => {
+
+};
+
+//GET /posts/:postID/comments/:commentID
+module.exports.comment_detail = (req, res, next) => {
+
+};
+
+//PUT /posts/:postID/comments/:commentID
+module.exports.edit_comment = (req, res, next) => {
+
+};
+
+//DELETE /posts/:postID/comments/:commentID
+module.exports.delete_comment = (req, res, next) => {
+
+};
+
+//PUT /posts/:postID/comments/:commentID/like
+module.exports.like_comment = (req, res, next) => {
+
+};
