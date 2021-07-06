00098f8ec0111e857ead56e15e54763dbd36b9b4
run-after <run.after@protonmail.com>
2021-07-06 13:26:52 -0500

    Add User model

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: {type: String, required: true, unique: true},
+  password: { type: String, required: true },
+  friends: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]// Array of friends
+});
+
+module.exports = mongoose.model('User', UserSchema);
