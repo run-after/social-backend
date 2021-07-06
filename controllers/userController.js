84eda2085c9ddf7518261e18b38ea1e29b3f3209
run-after <run.after@protonmail.com>
2021-07-06 14:27:10 -0500

    Add userController

---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+const User = require('../models/User');
+
+module.exports.user_list = (req, res, next) => {
+
+};
+
+module.exports.create_user = (req, res, next) => {
+
+};
+
+module.exports.user_details = (req, res, next) => {
+
+};
+
+module.exports.edit_user_details = (req, res, next) => {
+
+};
+
+module.exports.delete_user = (req, res, next) => {
+
+};
