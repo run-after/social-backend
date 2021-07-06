89bef26531d255ebe55ae9ba26e066305e156e50
run-after <run.after@protonmail.com>
2021-07-06 14:26:23 -0500

    Add index routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const indexController = require('../controllers/indexController');
+
+router.get('/', indexController.home);
+router.post('/login', indexController.log_in);
+router.post('/signup', indexController.sign_up);
 
 module.exports = router;
