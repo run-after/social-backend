6a24d1cad8eef687517e3e9f5b37c55c5055907b
run-after <run.after@protonmail.com>
2021-07-07 14:32:51 -0500

    Add passport middleware

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -15,6 +20,25 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    User.findOne({ email: username }, function (err, user) {
+      if (err) { return done(err); };
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      };
+      bcrypt.compare(password, user.password, function (err, res) {
+        if (res) {
+          return done(null, user);
+        } else {
+          console.log('wrong password')
+          return done(null, false, { message: 'Incorrect password.' });
+        };
+      });
+    });
+  }
+));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,6 +48,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -45,3 +70,12 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+// I can sign up
+// I can log in
+// password is hashed
+// password is compared
+
+// Need jwt
+// Need to save jwt
