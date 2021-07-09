e54f70b273e11f4cc91673febb1a3951f2a3c59f
run-after <run.after@protonmail.com>
2021-07-09 11:01:12 -0500

    Change log_in method to create and sign a web token for sign in

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 module.exports.home = (req, res, next) => {
   res.send('hi')
@@ -10,9 +11,10 @@ module.exports.log_in = (req, res, next) => {
   passport.authenticate('local', {session: false}, function (err, user, info) {
     if (err) { return next(err); };
     if (!user) { return res.json(info); };
-    req.logIn(user, {session: false},function (err) {
+    req.logIn(user, {session: false}, function (err) {
       if (err) { return next(err); };
-      return res.json({ 'message': 'logged in' });// maybe make token and save
+      const token = jwt.sign({user}, process.env.SECRET);
+      return res.json({ user, token });
     });
   })(req, res, next);
 };
