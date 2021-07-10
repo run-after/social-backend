1e4cb8fbe2095f7c28099f214855bc3d0eb49672
run-after <run.after@protonmail.com>
2021-07-10 13:38:58 -0500

    Add comment_list method

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,7 +2,10 @@ const Comment = require('../models/Comment');
 
 //GET /posts/:postID/comments
 module.exports.comment_list = (req, res, next) => {
-
+  Comment.find({'post': req.params.postID}).exec((err, comment_list) => {
+    if (err) { return res.json(err); };
+    return res.json(comment_list);
+  });
 };
 
 //POST /posts/:postID/comments
@@ -25,7 +28,12 @@ module.exports.delete_comment = (req, res, next) => {
 
 };
 
-//PUT /posts/:postID/comments/:commentID/like
+//GET /posts/:postID/comments/:commentID/likes
+module.exports.get_likes = (req, res, next) => {
+
+};
+
+//PUT /posts/:postID/comments/:commentID/likes
 module.exports.like_comment = (req, res, next) => {
 
 };
