a2dce9621b263fa8c6badd7487baf2acc53476f9
run-after <run.after@protonmail.com>
2021-07-10 11:02:28 -0500

    Add list_likes for comment/posts route

--- routes/posts.js
+++ routes/posts.js
@@ -9,6 +9,7 @@ router.post('/', postController.create_post);
 router.get('/:postID', postController.post_detail);
 router.put('/:postID', postController.edit_post);
 router.delete('/:postID', postController.delete_post);
+router.get('/:postID/likes', postController.get_likes);
 router.post('/:postID/likes', postController.like_post);
 
 router.get('/:postID/comments', commentController.comment_list);
@@ -16,6 +17,7 @@ router.post('/:postID/comments', commentController.create_comment);
 router.get('/:postID/comments/:commentID', commentController.comment_detail);
 router.put('/:postID/comments/:commentID', commentController.edit_comment);
 router.delete('/:postID/comments/:commentID', commentController.delete_comment);
+router.get('/:postID/comments/:commentID/likes', commentController.get_likes);
 router.put('/:postID/comments/:commentID/likes', commentController.like_comment);
 
 module.exports = router;
