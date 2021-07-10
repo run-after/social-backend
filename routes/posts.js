9c4bb31027d21e3aa49eab60d061c42843981922
run-after <run.after@protonmail.com>
2021-07-10 11:01:38 -0500

    Add like post/comment route

--- routes/posts.js
+++ routes/posts.js
@@ -9,13 +9,13 @@ router.post('/', postController.create_post);
 router.get('/:postID', postController.post_detail);
 router.put('/:postID', postController.edit_post);
 router.delete('/:postID', postController.delete_post);
-router.put('/:postID/like', postController.like_post)
+router.post('/:postID/likes', postController.like_post);
 
 router.get('/:postID/comments', commentController.comment_list);
 router.post('/:postID/comments', commentController.create_comment);
 router.get('/:postID/comments/:commentID', commentController.comment_detail);
 router.put('/:postID/comments/:commentID', commentController.edit_comment);
 router.delete('/:postID/comments/:commentID', commentController.delete_comment);
-router.put('/:postID/comments/:commentID/like', commentController.like_comment);
+router.put('/:postID/comments/:commentID/likes', commentController.like_comment);
 
 module.exports = router;
