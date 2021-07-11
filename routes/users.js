c3767ce0e92c528c92e9b3908da5073e770b2715
run-after <run.after@protonmail.com>
2021-07-11 13:59:14 -0500

    Add get_user_posts/comments routes

--- routes/users.js
+++ routes/users.js
@@ -7,5 +7,7 @@ router.get('/', userController.user_list);
 router.get('/:userID', userController.user_details);
 router.put('/:userID', userController.edit_user_details);
 router.delete('/:userID', userController.delete_user);
+router.get('/:userID/posts', userController.get_user_posts);
+router.get('/:userID/comments', userController.get_user_comments);
 
 module.exports = router;
