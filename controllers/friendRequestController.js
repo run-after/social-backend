9294eb641d7fa72ecd098ba10782d1a5f08ad91a
run-after <run.after@protonmail.com>
2021-07-21 10:25:02 -0500

    Add friend_request_list method

--- controllers/friendRequestController.js
+++ controllers/friendRequestController.js
@@ -1,5 +1,12 @@
 const FriendRequest = require('../models/FriendRequest');
 
+module.exports.friend_request_list = (req, res, next) => {
+  FriendRequest.find().exec((err, request_list) => {
+    if (err) { return res.json(err); };
+    return res.json(request_list);
+  });
+};
+
 module.exports.create_friend_request = (req, res, next) => {
   const friendRequest = new FriendRequest({
     requester: req.user,
