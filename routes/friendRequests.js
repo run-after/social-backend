b5c10cff92c2844590f19989a1865e7584dd9578
run-after <run.after@protonmail.com>
2021-07-21 10:25:27 -0500

    Add friend_request_list route

--- routes/friendRequests.js
+++ routes/friendRequests.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const friendRequestController = require('../controllers/friendRequestController');
 
+router.get('/', friendRequestController.friend_request_list);
 router.post('/:requestedID', friendRequestController.create_friend_request);
 router.delete('/:requestedID', friendRequestController.delete_friend_request);
 
