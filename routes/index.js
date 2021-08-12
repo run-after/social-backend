f260732dae2f66de49323a57ef1a5e705fdaa9fd
run-after <run.after@protonmail.com>
2021-08-12 10:59:42 -0500

    Add Facebook login route

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const indexController = require('../controllers/indexController');
 
 router.get('/', indexController.home);
 router.post('/login', indexController.log_in);
+router.post('/login/facebook', indexController.facebook_log_in);
 router.post('/users', indexController.create_user);
 
 module.exports = router;
