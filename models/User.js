16e99b345959b6ac908484088da7de338d293aa3
run-after <run.after@protonmail.com>
2021-08-19 12:19:32 -0500

    Add avatar image url to model

--- models/User.js
+++ models/User.js
@@ -8,7 +8,8 @@ const UserSchema = new Schema({
   password: { type: String, required: true },
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],// Array of friends
   isFacebookLogin: { type: Boolean, default: false },
-  facebookID: {type: String}
+  facebookID: { type: String },
+  avatar: {type: String, default: 'https://social-bucket.s3.us-east-2.amazonaws.com/anon.png'}
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
