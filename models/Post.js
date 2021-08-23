a4062a228a99852f9f5debc25f8f1d07e908493e
run-after <run.after@protonmail.com>
2021-08-23 14:25:01 -0500

    Add isPicture attribute to allow for image posts

--- models/Post.js
+++ models/Post.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
   content: { type: String, required: true },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  isPicture: { type: Boolean, defaultValue: false }
 }, { timestamps: true }
 );
 
